e96ade8797ee9b3e5c439512669bc2264fe28acf
Bharadwaj Kuna <bkuna@mandmdirect.com>
2023-11-26 18:25:07 +0000

    Hero Section in progress

--- help-page/src/Components/HeroSection/HeroSection.jsx
+++ help-page/src/Components/HeroSection/HeroSection.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import '../HeroSection/HeroSection.css'
+import { GoArrowRight } from "react-icons/go";
 
 const HeroSection = () => {
   return (
     <section id="main-HeroSection">
-      <div id="main-div-Hero">
+      <div class="main-HeroSection__container">
+
+        
         <h1> How can we help? </h1>
-        <input type='text' placeholder='Search'/>
+        <div class="main-HeroSection__input">
+          <input type='text' placeholder='Search'/>
+          <GoArrowRight />
+        </div>
+        
       </div>
+      
     </section>
   )
 }
