e32f5f28637d48f01fbe61fad2ea0beb0feb81f9
Kanchari-Kumara-Swamy <nanikanchari@gmail.com>
2023-11-26 17:43:34 +0530

    Initial commit

--- help-page/src/Components/HeroSection/HeroSection.jsx
+++ help-page/src/Components/HeroSection/HeroSection.jsx
@@ -3,9 +3,12 @@ import '../HeroSection/HeroSection.css'
 
 const HeroSection = () => {
   return (
-    <div id="main-HeroSection">
-      HeroSection
-    </div>
+    <section id="main-HeroSection">
+      <div id="main-div-Hero">
+        <h1> How can we help? </h1>
+        <input type='text' placeholder='Search'/>
+      </div>
+    </section>
   )
 }
 
