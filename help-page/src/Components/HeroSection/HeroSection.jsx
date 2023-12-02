6a94d6864d865efd14da1d1562e573ee54f44dd7
Kanchari-Kumara-Swamy <nanikanchari@gmail.com>
2023-12-02 19:48:07 +0530

    changes to css

--- help-page/src/Components/HeroSection/HeroSection.jsx
+++ help-page/src/Components/HeroSection/HeroSection.jsx
@@ -6,14 +6,11 @@ const HeroSection = () => {
   return (
     <section id="main-HeroSection">
       <div class="main-HeroSection__container">
-
-        
         <h1> How can we help? </h1>
         <div class="main-HeroSection__input">
           <input type='text' placeholder='Search'/>
           <GoArrowRight />
         </div>
-        
       </div>
       
     </section>
