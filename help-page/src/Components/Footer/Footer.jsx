3402324f2283205a32fdc0bcee8a314d9d3d8344
Bharadwaj Kuna <bkuna@mandmdirect.com>
2023-12-03 16:11:13 +0000

    Footer

--- help-page/src/Components/Footer/Footer.jsx
+++ help-page/src/Components/Footer/Footer.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import '../Footer/Footer.css'
+import { useState } from 'react'
+import useWindowSize from '../NavBar/hooks/useWindowSize';
 
 const Footer = () => {
+  const size = useWindowSize();
   return (
-    <div>
-      Footer
+
+    <>
+    
+    {size.width >1024 && (
+      <div>
+      <h1>Desktop Footer</h1>
     </div>
+    )}
+    
+    
+    {size.width < 1024 && (
+      <h1>Mobile Footer</h1>
+      )}
+    
+    </>
+    
+    
   )
 }
 
